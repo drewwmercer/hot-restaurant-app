4350fd0d990d0bf677736a5bacbc48ae609ae5f0
Drew Mercer <japanexpress@gmail.com>
2017-09-21 10:42:35 -0400

    Setting up Express server file

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+var diners = [];
