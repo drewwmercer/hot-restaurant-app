b7de5f89472c3f6b1a1485edfb3f88ca8c55ccb5
Drew Mercer <japanexpress@gmail.com>
2017-09-21 19:37:00 -0400

    Updating server comments

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Setting up the Express app
 var app = express();
 // var port = process.env.PORT || 3000;
 var port = 3000;
 
+// Setting up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+// The data
 var diners = [];
 
+// Routes
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
