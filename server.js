4ffef7957194c69c5f02adf15b7a2a1765303fe5
Drew Mercer <japanexpress@gmail.com>
2017-09-21 19:24:49 -0400

    Updated server at api/tables

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+var port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
