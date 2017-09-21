efe0c00f63100bfff4b92afd985212da965e6b7e
Drew Mercer <japanexpress@gmail.com>
2017-09-21 12:26:55 -0400

    Adding to server setup

--- server.js
+++ server.js
@@ -6,33 +6,36 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:"application/vnd.api+json"}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 var diners = [];
 
-app.get('/',function(req,res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/tables',function(req,res) {
-    res.sendFile(path.join(__dirname, 'tables.html'));
+app.get('/tables', function(req, res) {
+  res.sendFile(path.join(__dirname, 'tables.html'));
 });
 
-app.get('/reserve',function(req,res) {
-    res.sendFile(path.join(__dirname, 'reserve.html'));
+app.get('/reserve', function(req, res) {
+  res.sendFile(path.join(__dirname, 'reserve.html'));
 });
 
-app.get('/api/tables',function(req,res) {
-    for (var i = 0; i < 4; i++) {
-        res.json(diners[i]);
-    }
+app.get('/api/tables', function(req, res) {
+  for (var i = 0; i < 4; i++) {
+    res.json(diners[i]);
+  }
 });
 
-app.get('/api/waitlist',function(req,res) {
-    for (var i = 5; i < diners.length; i++) {
-        res.json(diners[i]);
-    }
+app.get('/api/waitlist', function(req, res) {
+  for (var i = 5; i < diners.length; i++) {
+    res.json(diners[i]);
+  }
 });
 
+app.listen(port, function() {
+  console.log("I'm listening on PORT " + port);
+});
