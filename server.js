1b23cb1a0db17a9451de010746156f48045b0553
Drew Mercer <japanexpress@gmail.com>
2017-09-21 12:24:54 -0400

    Adding to server setup

--- server.js
+++ server.js
@@ -16,3 +16,16 @@ app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/tables',function(req,res) {
+    res.sendFile(path.join(__dirname, 'tables.html'));
+});
+
+app.get('/reserve',function(req,res) {
+    res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/api/tables',function(req,res) {
+    for (var i =0; i<4; i++) {
+        res.json(diners[i]);
+    }
+});
