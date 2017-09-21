d7a3bdba9ca135d6afa4ca51bd822a4d4798c699
Drew Mercer <japanexpress@gmail.com>
2017-09-21 09:56:44 -0400

    Setting up Express server file

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
